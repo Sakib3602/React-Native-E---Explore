a927cbd80fe2e9f3b6ae6c9dcfb653f731d71e8e
Sakib3602 <sakibsarker6969@gmail.com>
2026-05-22 19:31:50 +0600

    native wind added and exploring is on

---
+++ app/(pr)/profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {  Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+export default function Profile() {
+  return (
+    <SafeAreaView>
+      <View >
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to Nativewind!
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
